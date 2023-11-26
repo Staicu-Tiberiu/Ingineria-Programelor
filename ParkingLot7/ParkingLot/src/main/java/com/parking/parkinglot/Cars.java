59494edbaf5fbae43071764252db19156282618d
Staicu-Tiberiu <tibistaicu59@gmail.com>
2023-11-26 23:45:30 +0200

    ParkingLot7
    ParkingLot7

---
+++ ParkingLot7/ParkingLot/src/main/java/com/parking/parkinglot/Cars.java
@@ -0,0 +1,30 @@
+package com.parking.parkinglot;
+
+import com.parking.parkinglot.common.CarDto;
+import com.parking.parkinglot.ejb.CarsBean;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Cars", value = "/Cars")
+public class Cars extends HttpServlet {
+    @Inject
+    CarsBean carsBean;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<CarDto> cars=carsBean.findAllCars();
+        request.setAttribute("cars",cars);
+        request.setAttribute("numberOfFreeParkingSpots", 10);
+        request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
